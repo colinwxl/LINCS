52a62bac05623854b0ac77c12f3003b194a99074
Greg <ggundersen@gmail.com>
2016-06-01 13:26:49 -0400

    Removing server-es5

--- server-es5/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* eslint global-require:0 */
-// Load a different file in webpack depending on the NODE_ENV environemt variable.
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./main.prod');
-} else {
-  module.exports = require('./main.dev');
-}
