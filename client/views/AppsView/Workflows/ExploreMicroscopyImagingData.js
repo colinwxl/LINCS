63f8fc1b00757e1aec1e82c8ddaef49f8ea2324b
Edward He <edward.he@macaulay.cuny.edu>
2016-08-15 10:04:13 -0400

    Reword HMS portion of LINCS Workflow.

--- client/views/AppsView/Workflows/ExploreMicroscopyImagingData.js
+++ client/views/AppsView/Workflows/ExploreMicroscopyImagingData.js
@@ -30,14 +30,15 @@ export default class Workflow extends Component {
               </p>
               <div className={styles.section}>
                 <p>
-                  The <strong><a href="http://lincs.hms.harvard.edu" target="_blank">Harvard Medical School LINCS center
-                  </a></strong> collect various types of imaging data such as florescence imaging of cell viability and proliferation through apototic and mitotic markers.
-                  In addition, HMS LINCS monitors the activity of key protein kinases using florescence microscopy and
-                  some of those experiments are conducted in live cells. The best place to obtain
-                  access to these datasets is through the <a href="http://lincs.hms.harvard.edu/db" target="_blank">
-                  HMS LINCS Database</a>. Collected images are
+                  The <strong><a href="http://lincs.hms.harvard.edu" target="_blank">HMS-LINCS
+                  </a> center </strong>collect various types of imaging data such as florescence imaging of cell viability and
+                  proliferation through apototic and mitotic markers. HMS-LINCS, additionally, monitors the activity of
+                  key protein kinases using florescence microscopy and some of those experiments are conducted in
+                  live cells. The best place to obtain access to these datasets is through the
+                  <a href="http://lincs.hms.harvard.edu/db" target="_blank"> HMS-LINCS Database</a>. Collected images are
                   made available through the <a href="https://lincs-omero.hms.harvard.edu/webclient/userdata/?experimenter=-1" target="_blank">
-                  OMERO</a> web-client. See for example this link and image below: <a href="https://lincs-omero.hms.harvard.edu/webclient/img_detail/317001/" target="_blank">https://lincs-omero.hms.harvard.edu/webclient/img_detail/317001/</a>
+                   OMERO</a> web-client. See for example this link and image below: <br/>
+                  <a href="https://lincs-omero.hms.harvard.edu/webclient/img_detail/317001/" target="_blank">https://lincs-omero.hms.harvard.edu/webclient/img_detail/317001/</a>
                 </p>
                 <div>
                   <img src={omeroScreenshot} alt="OMERO screenshot" />
