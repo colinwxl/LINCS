7f529463bde8ce86d61231d957e0b2c5b75fc6b5
Edward He <edward.he@macaulay.cuny.edu>
2016-09-01 15:26:48 -0400

    Add Story20160720.

--- client/views/NewsView/index.js
+++ client/views/NewsView/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PageBanner from 'components/PageBanner';
 import styles from './NewsView.scss';
 
+import Story20160720 from './Stories/Story20160720.js';
 import Story20160616 from './Stories/Story20160616.js';
 import Story20160602 from './Stories/Story20160602.js';
 import Story20160523 from './Stories/Story20160523.js';
@@ -45,6 +46,7 @@ export default function NewsView() {
       <div className="container">
         <div className="row">
           <div className={`col-xs-12 col-xl-9 ${styles.news}`}>
+            <Story20160720 />
             <Story20160616 />
             <Story20160602 />
             <Story20160523 />
