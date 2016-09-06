f0902c332329a2b9a0db911fb203ddf228991193
Edward He <edward.he@macaulay.cuny.edu>
2016-09-06 11:37:56 -0400

    Begin Map -> MapInfo redux pipeline.

---
+++ client/components/InteractiveMap/MapInfo/index.js
@@ -0,0 +1,22 @@
+import React, { Component, propTypes } from 'react';
+import styles from './MapInfo.scss';
+
+
+export default class MapInfo extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <p>
+        Hello world!
+      </p>
+    );
+  }
+}
+
+MapInfo.propTypes = {
+
+}
