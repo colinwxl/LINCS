5d0dec9c819fa93a4f00589723f30461ab0852f8
Edward He <edward.he@macaulay.cuny.edu>
2016-09-13 12:17:00 -0400

    Fix linter errors.

--- client/views/AppsView/Workflows/ExploreMicroscopyImagingData.js
+++ client/views/AppsView/Workflows/ExploreMicroscopyImagingData.js
@@ -31,13 +31,21 @@ export default class Workflow extends Component {
               <div className={styles.section}>
                 <p>
                   The <strong><a href="http://lincs.hms.harvard.edu" target="_blank">HMS LINCS Center</a></strong> collects
-                  various types of fixed- and live-cell fluorescence imaging data to assess cell viability,
-                  cell proliferation, and the abundance and activity of key proteins. These imaging datasets,
-                  including raw and processed image data as well as derived numerical data, are available through
-                  the <a href="http://lincs.hms.harvard.edu/db" target="_blank"> HMS-LINCS Database</a> and the
-                  associated <a href="https://lincs-omero.hms.harvard.edu/webclient/userdata/?experimenter=-1" target="_blank">
-                  HMS LINCS OMERO</a> server. An <a href="https://lincs-omero.hms.harvard.edu/webclient/img_detail/1375309/"
-                  target="_blank">example image</a> from the HMS LINCS OMERO server is shown below.
+                  various types of fixed- and live-cell fluorescence imaging data to assess cell
+                  viability, cell proliferation, and the abundance and activity of key proteins.
+                  These imaging datasets, including raw and processed image data as well as
+                  derived numerical data, are available through the&nbsp;
+                  <a href="http://lincs.hms.harvard.edu/db" target="_blank">HMS-LINCS Database</a>
+                  &nbsp;and the associated&nbsp;
+                  <a href="https://lincs-omero.hms.harvard.edu/webclient/userdata/?experimenter=-1" target="_blank">
+                    HMS LINCS OMERO
+                  </a> server. An&nbsp;
+                  <a
+                    href="https://lincs-omero.hms.harvard.edu/webclient/img_detail/1375309/"
+                    target="_blank"
+                  >
+                    example image
+                  </a> from the HMS LINCS OMERO server is shown below.
                 </p>
                 <div>
                   <img src={omeroScreenshot} alt="OMERO screenshot" />
