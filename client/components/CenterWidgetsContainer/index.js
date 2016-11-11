425e039da1a038d5a06034d10f207d6bfadfc29b
Edward He <edward.he@macaulay.cuny.edu>
2016-11-11 16:27:16 -0500

    Setting up structure for CenterWidgetsContaienr.

--- client/components/CenterWidgetsContainer/index.js
+++ client/components/CenterWidgetsContainer/index.js
@@ -1,2 +1,18 @@
-import React, { PropTypes, Component } from 'react';
 import styles from './CenterWidgetsContainer.scss';
+import CenterInfoWidget from '../CenterInfoWidget';
+
+import centersInfo from './centers_info.json';
+
+export default function CenterWidgetsContainer() {
+  const centers = centersInfo.map(center => {
+    return {
+      <CentersInfoWidget center={center} />
+    }
+  });
+
+  return (
+      <div className={styles['center-widgets-container']}>
+        {centers}
+      </div>
+  );
+};
