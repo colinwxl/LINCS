6a75116f1881f7d73dba989a193b4e7d8568c6bc
Edward He <edward.he@macaulay.cuny.edu>
2016-11-12 17:17:40 -0500

    Add more seed data.

--- client/components/CenterWidgetsContainer/centers_info.js
+++ client/components/CenterWidgetsContainer/centers_info.js
@@ -17,4 +17,28 @@ export default [
     iconUrl: 'http://google.com',
     description: 'This is a test description3',
   },
+  {
+    name: 'Test4',
+    url: 'http://google.com',
+    iconUrl: 'http://google.com',
+    description: 'This is a test description4',
+  },
+  {
+    name: 'Test5',
+    url: 'http://google.com',
+    iconUrl: 'http://google.com',
+    description: 'This is a test description5',
+  },
+  {
+    name: 'Test6',
+    url: 'http://google.com',
+    iconUrl: 'http://google.com',
+    description: 'This is a test description6',
+  },
+  {
+    name: 'Test7',
+    url: 'http://google.com',
+    iconUrl: 'http://google.com',
+    description: 'This is a test description7',
+  },
 ];
