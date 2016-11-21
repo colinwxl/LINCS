f6cc00fdf4e47497dc0bc9bb55c14cd2fe39d1e0
Edward He <edward.he@macaulay.cuny.edu>
2016-11-21 12:22:49 -0500

    Begin onClick handler for imageMap before coordinate recalc.

--- client/views/CentersView/Overview/index.js
+++ client/views/CentersView/Overview/index.js
@@ -15,20 +15,30 @@ export default class Overview extends Component {
     super(props);
     this.state = {
       mapTab: 'staticMap',
+      highlightedCards: [],
     };
   }
 
   componentDidMount() {
-    // console.log(this.state);
     const { mapTab } = this.state;
     const isStaticMap = mapTab === 'staticMap';
     if (isStaticMap) {
-      console.log($('img[usemap]'));
+      // $('img[usemap]').rwdImageMaps()
+    }
+  }
+
+  componentDidUpdate() {
+    const { mapTab } = this.state;
+    const isStaticMap = mapTab === 'staticMap';
+    if (isStaticMap) {
+      window.$('img[usemap]').rwdImageMaps()
     }
   }
 
   handleSMClicked = () => { this.setState({ mapTab: 'staticMap' }); }
   handleMWClicked = () => { this.setState({ mapTab: 'mapWidget' }); }
+
+  handleSpotClick = () => { this.setState({ mapTab: 'mapWidget' }); }
 
   render() {
     const { mapTab } = this.state;
@@ -93,21 +103,8 @@ export default class Overview extends Component {
                         <div>
                           <img src={centersMapImg} alt="Map of LINCS Centers" useMap="#Map" />
                           <map name="Map" id="Map">
-                            <area alt="" title="" href="#" shape="poly" coords="263,142,267,144,272,143,274,140,272,136,270,133,267,132,263,136,263,139" />
-                            <area alt="" title="" href="#" shape="poly" coords="247,190,254,189,260,182,262,189,271,189,264,195,267,208,258,201,251,206,254,196" />
-                            <area alt="" title="" href="#" shape="poly" coords="212,359,215,355,212,350,206,349,203,352,203,354,207,358,210,361" />
-                            <area alt="" title="" href="#" shape="poly" coords="218,376,219,378,220,382,220,385,216,388,213,387,211,384,211,380,213,377" />
-                            <area alt="" title="" href="#" shape="poly" coords="252,466,249,464,248,459,248,456,249,454,253,454,257,455,257,459,257,462,256,465" />
-                            <area alt="" title="" href="#" shape="poly" coords="268,484,276,484,280,475,281,482,290,484,285,488,287,499,280,495,272,499,275,490" />
-                            <area alt="" title="" href="#" shape="poly" coords="700,626,704,625,705,620,705,618,703,615,699,614,698,616,696,618,695,621,697,623" />
-                            <area alt="" title="" href="#" shape="poly" coords="1016,686,1017,683,1018,678,1017,673,1011,673,1009,675,1006,677,1006,679,1006,683,1009,685,1011,688" />
-                            <area alt="" title="" href="#" shape="poly" coords="884,394,886,390,886,387,882,384,878,383,875,385,874,388,875,391,877,392,879,392,880,395" />
-                            <area alt="" title="" href="#" shape="poly" coords="1027,360,1030,360,1030,357,1031,353,1030,350,1026,349,1022,350,1022,354,1021,359" />
-                            <area alt="" title="" href="#" shape="poly" coords="1060,339,1063,338,1065,335,1065,331,1063,329,1059,327,1056,329,1055,333,1056,337" />
-                            <area alt="" title="" href="#" shape="poly" coords="1058,317,1064,309,1072,316,1069,305,1075,299,1067,299,1063,289,1060,298,1052,299,1059,304,1055,316" />
-                            <area alt="" title="" href="#" shape="poly" coords="1072,277,1077,273,1077,267,1072,263,1069,265,1066,268,1068,273" />
-                            <area alt="" title="" href="#" shape="poly" coords="1082,277,1090,270,1100,278,1097,267,1102,260,1094,260,1090,251,1087,258,1079,260,1087,268,1089,260" />
-                            <area alt="" title="" href="#" shape="poly" coords="1100,270,1105,270,1110,261,1113,268,1122,269,1116,275,1120,286,1109,279,1103,286,1105,275" />
+                            <area onClick={this.handleSpotClick} alt="" title="" shape="circle" coords="267,137,8"/>
+                            <area onClick={this.handleSpotClick} alt="" title="" shape="rect" coords="0,0,82,126" />
                           </map>
                         </div> :
                         <CenterWidgetsContainer />
