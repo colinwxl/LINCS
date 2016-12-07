06ee849e9391517d3f60430cd0956f678e065852
Edward He <edward.he@macaulay.cuny.edu>
2016-12-07 16:28:34 -0500

    Add links to each news dropdown.

--- client/views/NewsView/Stories/Story20161207.js
+++ client/views/NewsView/Stories/Story20161207.js
@@ -23,18 +23,17 @@ export default function Story20161207() {
       <Collapsible trigger="Research Highlights">
         <ul>
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113434.1" target="_blank">
               Predicting Adverse Cardiovascular Events for Tyrosine Kinase
-              Inhibitors from Molecular Features (Podium Presentation by
-              Anders Dohlman)
-            </p>
+              Inhibitors from Molecular Features
+            </a> (Podium Presentation by Anders Dohlman)
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.1038/ncomms12846" target="_blank">
               Extraction and Analysis of Signatures from the Gene Expression
               Omnibus by the Crowd
-            </p>
+            </a>
           </li>
         </ul>
       </Collapsible>
@@ -42,30 +41,30 @@ export default function Story20161207() {
       <Collapsible trigger="Data Commons">
         <ul>
           <li>
-            <p>
+            <a href="http://doi.org/10.6084/m9.figshare.4244216" target="_blank">
               FAIR Dataset Landing Pages, Digital Research Objects,
               and Software Tools for LINCS and BD2K
-            </p>
+            </a>
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113424.1" target="_blank">
               Cloud-Based Drag-and-Drop Scalable RNA Sequencing Pipeline
-            </p>
+            </a>
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.1093/database/baw100" target="_blank">
               The Harmonizome:  A Collection of Processed Datasets
               Gathered to Serve and Mine Knowledge about Genes
-            </p>
+            </a>
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113423.1" target="_blank">
               RNA-Seq Pipeline Tutorial with an Example of Reprocessing
               Data from a Recent Zika Virus Study
-            </p>
+            </a>
           </li>
 
           <li>
@@ -79,10 +78,10 @@ export default function Story20161207() {
       <Collapsible trigger="Standards Development">
         <ul>
           <li>
-            <p>
+            <a href="http://doi.org/10.6084/m9.figshare.4244180" target="_blank">
               Integrative Representation and Analysis of the LINCS Cell
               lines using the Cell Line Ontology
-            </p>
+            </a>
           </li>
         </ul>
       </Collapsible>
@@ -90,9 +89,9 @@ export default function Story20161207() {
       <Collapsible trigger="Training and Workforce Development">
         <ul>
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113421.1" target="_blank">
               Community Training and Outreach Activities of the BD2K-LINCS DCIC
-            </p>
+            </a>
           </li>
         </ul>
       </Collapsible>
@@ -100,18 +99,19 @@ export default function Story20161207() {
       <Collapsible trigger="BioCADDIE and Resource indexing">
         <ul>
           <li>
-            <p>
+            <a href="http://doi.org/10.6084/m9.figshare.4244210" target="_blank">
               The LINCS Data Portal and FAIR LINCS Dataset Landing Pages
-            </p>
+            </a>
           </li>
         </ul>
 
         <ul>
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113420.1" target="_blank">
               Datasets2Tools:
-              Enriching DataMed with Canned Analyses (Podium Presentation by Denis Torre)
-            </p>
+              Enriching DataMed with Canned Analyses
+            </a>
+            (Podium Presentation by Denis Torre)
           </li>
         </ul>
       </Collapsible>
@@ -119,35 +119,36 @@ export default function Story20161207() {
       <Collapsible trigger="Software, Analysis, and Methods Development">
         <ul>
           <li>
-            <p>
+            <a href="http://doi.org/10.6084/m9.figshare.4244408.v1" target="_blank">
               GRcalculator: An Online Tool for Calculating and Mining Drug Response Data
-            </p>
+            </a>
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113430.1" target="_blank">
               Clustergrammer: Interactive Visualization and Analysis
               Tool for High-Dimensional Biological Data
-            </p>
+            </a>
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113431.1" target="_blank">
               Dashboard Visualization and Tool Integration for Enrichr
-            </p>
+            </a>
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113433.1" target="_blank">
             GEN3VA: Aggregation and Analysis of Gene Expression
-            Signatures from Related Studies (Podium Presentation by Avi Ma’ayan)
-            </p>
+            Signatures from Related Studies
+            </a>
+            (Podium Presentation by Avi Ma’ayan)
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.5281/zenodo.167645" target="_blank">
             Integrative LINCS (iLincs): Connecting Diseases, Drugs and Mechanisms of Actions
-            </p>
+            </a>
           </li>
 
           <li>
@@ -158,22 +159,22 @@ export default function Story20161207() {
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.5281/zenodo.167644" target="_blank">
               Computational Tools and Resources for LINCS Proteomics Data
-            </p>
+            </a>
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.7490/f1000research.1113436.1" target="_blank">
               KEGGlincs Design and Application:
               An R Package for Exploring Relationships in Biological Pathways
-            </p>
+            </a>
           </li>
 
           <li>
-            <p>
+            <a href="http://doi.org/10.6084/m9.figshare.4244204" target="_blank">
               Multitask Deep Neural Net Kinase Activity Profiler
-            </p>
+            </a>
           </li>
 
         </ul>
@@ -182,9 +183,9 @@ export default function Story20161207() {
       <Collapsible trigger="Collaborative Presentations">
         <ul>
           <li>
-            <p>
+            <a href="http://doi.org/10.6084/m9.figshare.4244213" target="_blank">
               FAIR LINCS Data and Metadata powered by the CEDAR Framework
-            </p>
+            </a>
           </li>
         </ul>
       </Collapsible>
