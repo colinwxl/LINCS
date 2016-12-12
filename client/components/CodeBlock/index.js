a3635f57c6dcdb3a171410fd49732b13e8f58515
Edward He <edward.he@macaulay.cuny.edu>
2016-12-12 18:30:58 -0500

    Add dependency for React-Markdown.

---
+++ client/components/CodeBlock/index.js
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+const hljs = window.hljs;
+
+export default class CodeBlock extends Component {
+  componentDidMount = () => {
+    this.highlightCode();
+  }
+
+  componentDidUpdate = () => {
+    this.highlightCode();
+  }
+
+  highlightCode = () => {
+    hljs.highlightBlock(this.refs.code);
+  }
+
+  render() {
+    return (
+      <pre>
+        <code className={this.props.language} ref="code">
+          {this.props.literal}
+        </code>
+      </pre>
+    );
+  }
+}
+
+CodeBlock.propTypes = {
+  literal: PropTypes.string,
+  language: PropTypes.string,
+};
