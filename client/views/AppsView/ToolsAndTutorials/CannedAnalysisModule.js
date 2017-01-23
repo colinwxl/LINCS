e954d23904bfed5c4e8fb2916dff3ca926f4d2cc
Edward He <edward.he@macaulay.cuny.edu>
2017-01-23 15:09:32 -0500

    Initial iteration of canned analysis table.

--- client/views/AppsView/ToolsAndTutorials/CannedAnalysisModule.js
+++ client/views/AppsView/ToolsAndTutorials/CannedAnalysisModule.js
@@ -5,28 +5,39 @@ import cannedAnalysisSeed from './canned_analysis_seed.json';
 
 const analyses = cannedAnalysisSeed;
 
-// "dataset_accession": "LDS-1106",
-// "dataset_url": "http://lincsportal.ccs.miami.edu/datasets/#/view/LDS-1106",
-// "tool_name": "Drug-Pathway Browser",
-// "canned_analysis_url": "http://lincs.hms.harvard.edu/explore/pathway/",
-// "canned_analysis_description":
-
-// function priceFormatter(cell, row) {
-//   return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
-// }
-
 const options = {
-  sizePerPageList: [
-    { text: '5', value: 5 },
-    { text: '10', value: 10 },
-    { text: 'All', value: analyses.length },
-  ],
+  hideSizePerPage: true,
   sizePerPage: 5,
+  searchDelayTime: 250,
+};
+
+const toolNameLogoMap = {
+  'Drug-Pathway Browser': 'http://lincsproject.org/LINCS/files/tools_logos/explore2.jpg',
+  'Drug Response Browser': 'http://lincsproject.org/LINCS/files/tools_logos/explore4.jpg',
+  iLINCS: 'http://lincsproject.org/LINCS/files/tools_logos/i-lincs.png',
+  GEN3VA: 'http://lincsproject.org/LINCS/files/tools_logos/gen3va-logo.png',
+  'LINCS Data Explorer': 'http://lincsproject.org/LINCS/files/tools_logos/mep-lincs-data-explorer.png',
+  'RTK Profile Browser': 'http://lincsproject.org/LINCS/files/tools_logos/scatterplot_widget.jpg',
 };
 
 const format = (cell, row) => {
-  // can return a react component here
-  return `<img src="https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_1280.jpg" /> ${cell}`
+  const toolName = row.tool_name;
+  const toolLogo = toolNameLogoMap[toolName];
+  // const datasetUrl = row.dataset_url;
+  const cannedAnalysisUrl = row.canned_analysis_url;
+
+  return (
+    <div className={styles['tool-box']}>
+      <a href={cannedAnalysisUrl} target="_blank">
+        <div className={styles['tool-img-wrap']}>
+          <div className={styles['tool-img-inner']}>
+            <img src={toolLogo} alt={toolName} />
+          </div>
+        </div>
+        <span className={styles.toolTitle}>{toolName}</span>
+      </a>
+    </div>
+  );
 };
 
 export default function CannedAnalysisModule() {
@@ -37,8 +48,8 @@ export default function CannedAnalysisModule() {
         options={options}
         striped
         hover
-        pagination
         condensed
+        pagination
         search
       >
         <TableHeaderColumn
@@ -52,11 +63,12 @@ export default function CannedAnalysisModule() {
         </TableHeaderColumn>
         <TableHeaderColumn
           dataField="tool_name"
+          dataAlign="center"
           width="120"
           dataSort
           dataFormat={format}
         >
-          Tool Name
+          Tool
         </TableHeaderColumn>
         <TableHeaderColumn
           dataField="canned_analysis_description"
