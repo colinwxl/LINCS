7823e79bb2f41a0d418d2efe68ec9fe538faeb73
Edward He <edward.he@macaulay.cuny.edu>
2017-01-23 13:34:56 -0500

    Setting up Canned Analysis table.

--- client/views/AppsView/ToolsAndTutorials/CannedAnalysisModule.js
+++ client/views/AppsView/ToolsAndTutorials/CannedAnalysisModule.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import styles from '../AppsView.scss';
 import cannedAnalysisSeed from './canned_analysis_seed.json';
 
+const analyses = cannedAnalysisSeed;
+
+// "dataset_accession": "LDS-1106",
+// "dataset_url": "http://lincsportal.ccs.miami.edu/datasets/#/view/LDS-1106",
+// "tool_name": "Drug-Pathway Browser",
+// "canned_analysis_url": "http://lincs.hms.harvard.edu/explore/pathway/",
+// "canned_analysis_description":
+
+// function priceFormatter(cell, row) {
+//   return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+// }
+
+const options = {
+  sizePerPageList: [
+    { text: '5', value: 5 },
+    { text: '10', value: 10 },
+    { text: 'All', value: analyses.length },
+  ],
+  sizePerPage: 5,
+};
+
+const format = (cell, row) => {
+  // can return a react component here
+  return `<img src="https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_1280.jpg" /> ${cell}`
+};
+
 export default function CannedAnalysisModule() {
-  const analyses = cannedAnalysisSeed;
+  if (analyses && analyses.length > 0) {
+    return (
+      <BootstrapTable
+        data={analyses}
+        options={options}
+        striped
+        hover
+        pagination
+        condensed
+        search
+      >
+        <TableHeaderColumn
+          dataField="dataset_accession"
+          width="110"
+          dataAlign="center"
+          isKey
+          dataSort
+        >
+          Dataset Accession
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          dataField="tool_name"
+          width="120"
+          dataSort
+          dataFormat={format}
+        >
+          Tool Name
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          dataField="canned_analysis_description"
+        >
+          Canned Analysis Description
+        </TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
   return (
     <div className="row">
       <h3 className={styles['section-title']}>Canned Analysis</h3>
