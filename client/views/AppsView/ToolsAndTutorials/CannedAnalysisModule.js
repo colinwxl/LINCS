3da34a32fd06eaa09389ce417c601d1807cb6404
Edward He <edward.he@macaulay.cuny.edu>
2017-01-23 11:16:28 -0500

    Canned Analysis JSON ready.

--- client/views/AppsView/ToolsAndTutorials/CannedAnalysisModule.js
+++ client/views/AppsView/ToolsAndTutorials/CannedAnalysisModule.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from '../AppsView.scss';
+import cannedAnalysisSeed from './canned_analysis_seed.json';
 
 export default function CannedAnalysisModule() {
-  // Plan is to parse denis's file into json and load it in via a table here
+  const analyses = cannedAnalysisSeed;
   return (
     <div className="row">
       <h3 className={styles['section-title']}>Canned Analysis</h3>
       <h5 className="m-t-3 text-xs-center">
-        No tutorials found. Please try again later.
+        No analysis found. Please try again later.
       </h5>
     </div>
   );
