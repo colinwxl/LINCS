ac1849ceb7ee27e8c094554432e2324619dc9923
Edward He <edward.he@macaulay.cuny.edu>
2017-01-30 15:31:35 -0500

    Create Mobile page.

--- client/views/MobileDownloadView/index.js
+++ client/views/MobileDownloadView/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router';
-// import styles from './MobileDownloadView.scss';
+import styles from './MobileDownloadView.scss';
+import screenshot from './screenshot.png';
 
 export default function MobileDownloadView() {
   return (
-    <div className="container text-xs-center">
-      <h1>Page Not Found (404)</h1>
-      <Link to="/">Back To Home</Link>
+    <div className={styles.wrapper}>
+      <div className={'container'}>
+        <div className={styles['mobile-page']}>
+          <div className={`row ${styles.bordered}`}>
+            <h1 className="col-md-12">Mobile</h1>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <ul className={`list-unstyled list-inline ${styles.icons}`}>
+                <li><i className="fa fa-laptop fa-3x"></i></li>
+                <li><i className="fa fa-tablet fa-3x"></i></li>
+                <li><i className="fa fa-mobile fa-3x"></i></li>
+              </ul>
+              <h4>Take LINCS with you</h4>
+              <p>
+                With the LINCS Project Mobile App, you can explore LINCS centers,
+                resources, publications, and tools.
+              </p>
+              <ul className={`list-unstyled ${styles['app-store-links']}`}>
+                <li>
+                  <i className="fa fa-apple fa-2x"></i>
+                  <a href="https://itunes.apple.com/us/app/lincs-project-mobile-app/id1193096130?mt=8" target="_blank">Free for
+                    iPhone
+                  </a>
+                </li>
+                <li>
+                  <i className="fa fa-android fa-2x"></i>
+                  <a href="https://play.google.com/store/apps/details?id=com.lincsprojectmobile&hl=en" target="_blank">Free for Android</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6">
+              <img src={screenshot} alt="Screenshot" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
