fc7e4b0c8f8d84eb9b6035fefa462db1726f8c6e
Edward He <edward.he@macaulay.cuny.edu>
2017-02-22 18:33:58 -0500

    Update tooltip based on screensize.

--- client/views/CommunityView/Overview/AACR/AACRMeetingBox/index.js
+++ client/views/CommunityView/Overview/AACR/AACRMeetingBox/index.js
@@ -1,77 +1,92 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactTooltip from 'react-tooltip';
-import { Link } from 'react-router';
+// import { Link } from 'react-router';
 
 import styles from './AACRMeetingBox.scss';
 
-// {
-//   time: '5:00 - 5:15 PM',
-//   talkTitle: 'Introduction to the NIH LINCS Program',
-//   speaker: 'Peter K. Sorger PhD',
-//   speakerImg: sorger,
-//   centerName: 'HMS LINCS',
-//   centerPath: 'LINCS/centers/data-and-signature-generating-centers/hms-lincs',
-//   abstractLink: 'http://www.abstractsonline.com/pp8/#!/4292/presentation/11723',
-// },
+export default class AACRMeetingBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default function AACRMeetingBox(props) {
-  const { scheduleItem } = props;
-  return (
-    <div className={styles.widget}>
-      <div className={`${styles['widget-inner']}`}>
-        <p className={styles['widget-description']}>
-          <div className={`${styles.presenter} ${styles['inline-img-left']}`}>
-            <img
-              src={scheduleItem.speakerImg}
-              className={`${styles.thumbnail}`}
-              alt={scheduleItem.speaker}
-            />
-          </div>
-          <i>{scheduleItem.speaker}</i><br />
-          <strong>Time:</strong> <i>{scheduleItem.time}</i>
-          <br />
-          <div className={styles['desc-limit']}>
-            <a
-              href={scheduleItem.abstractLink}
-              target="_blank"
-              className={`${styles['no-style-link']}`}
-            >
+  componentWillMount() {
+    this.updateDimensions();
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions = () => {
+    const w = window;
+    const d = document;
+    const documentElement = d.documentElement;
+    const body = d.getElementsByTagName('body')[0];
+    const width = w.innerWidth || documentElement.clientWidth || body.clientWidth;
+    const height = w.innerHeight || documentElement.clientHeight || body.clientHeight;
+    this.setState({ width, height });
+  }
+
+  render() {
+    const { scheduleItem } = this.props;
+    return (
+      <div className={styles.widget}>
+        <div className={`${styles['widget-inner']}`}>
+          <p className={styles['widget-description']}>
+            <div className={`${styles.presenter} ${styles['inline-img-left']}`}>
+              <img
+                src={scheduleItem.speakerImg}
+                className={`${styles.thumbnail}`}
+                alt={scheduleItem.speaker}
+              />
+            </div>
+            <i>{scheduleItem.speaker}</i><br />
+            <strong>Time:</strong> <i>{scheduleItem.time}</i>
+            <br />
+            <div className={styles['desc-limit']}>
+              <a
+                href={scheduleItem.abstractLink}
+                target="_blank"
+                className={`${styles['no-style-link']}`}
+              >
+                {
+                  scheduleItem.talkTitle.length > 65 && this.state.width >= 1200 ?
+                    <span
+                      data-tip="Information is not available at this time."
+                      data-for={scheduleItem.time}
+                    >
+                      {scheduleItem.talkTitle.slice(0, 62)}...
+                    </span>
+                    : <span>{scheduleItem.talkTitle}</span>
+                }
+              </a>
               {
-                scheduleItem.talkTitle.length > 65 ?
-                  <span
-                    data-tip="Information is not available at this time."
-                    data-for={scheduleItem.time}
+                this.state.width >= 1200 ?
+                  <ReactTooltip
+                    id={scheduleItem.time}
+                    place="right"
+                    type="dark"
+                    effect="float"
                   >
-                    {scheduleItem.talkTitle.slice(0, 62)}...
-                  </span>
-                  : <span>{scheduleItem.talkTitle}</span>
+                    <div style={{ maxWidth: '10rem' }}>
+                      {scheduleItem.talkTitle}
+                    </div>
+                  </ReactTooltip>
+                : null
               }
-            </a>
-            <ReactTooltip
-              id={scheduleItem.time}
-              place="right"
-              type="dark"
-              effect="float"
-            >
-              <div style={{ maxWidth: '10rem' }}>
-                {scheduleItem.talkTitle}
-              </div>
-            </ReactTooltip>
-          </div>
-          <br />
-        </p>
+            </div>
+            <br />
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-// <i
-//   className={`fa fa-info-circle ${styles['info-icon']}`}
-//   aria-hidden="true"
-//   data-tip="Information is not available at this time."
-//   data-for={center.name}
-// />
-
 
 AACRMeetingBox.propTypes = {
   scheduleItem: PropTypes.object,
