5d625622034b9e8857e0c42769e82640c9db4e35
Edward He <edward.he@macaulay.cuny.edu>
2017-04-24 17:43:43 -0400

    Begin creating canned_analyses table.

---
+++ migrations/20170424_create_canned_analyses_table.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE `challenges` (
+--   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+--   `title` varchar(255) NOT NULL,
+--   `sub_title` varchar(255) DEFAULT NULL,
+--   `description` mediumtext,
+--   `start_date` datetime NOT NULL,
+--   `end_date` datetime DEFAULT NULL,
+--   `key_links` text,
+--   `created_at` datetime NOT NULL,
+--   `updated_at` datetime DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=91 DEFAULT CHARSET=utf8;
+
+CREATE TABLE canned_analyses
