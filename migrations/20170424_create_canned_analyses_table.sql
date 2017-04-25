7da700d4588d77b8fb212e0527634b271182c9c5
Edward He <edward.he@macaulay.cuny.edu>
2017-04-25 11:07:44 -0400

    Create C.A. table migrations.

--- migrations/20170424_create_canned_analyses_table.sql
+++ migrations/20170424_create_canned_analyses_table.sql
@@ -1,14 +1,14 @@
--- CREATE TABLE `challenges` (
---   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
---   `title` varchar(255) NOT NULL,
---   `sub_title` varchar(255) DEFAULT NULL,
---   `description` mediumtext,
---   `start_date` datetime NOT NULL,
---   `end_date` datetime DEFAULT NULL,
---   `key_links` text,
---   `created_at` datetime NOT NULL,
---   `updated_at` datetime DEFAULT NULL,
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB AUTO_INCREMENT=91 DEFAULT CHARSET=utf8;
-
-CREATE TABLE canned_analyses
+CREATE TABLE `canned_analyses` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `subtitle` varchar(255) NOT NULL,
+  `grouping` varchar(255) NOT NULL,
+  `subgrouping` varchar(255) DEFAULT NULL,
+  `dataset_accessions_list` varchar(255) NOT NULL,
+  `tool_name` int(10) NOT NULL,
+  `tool_url` int(10) NOT NULL,
+  `canned_analysis_url` varchar(255) NOT NULL,
+  `canned_analysis_description` varchar(255) NOT NULL,
+  `metadata` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
