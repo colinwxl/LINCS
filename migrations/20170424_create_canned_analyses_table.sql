e4feb0c75bfa67792ad99e865e29563f76a813f9
Edward He <edward.he@macaulay.cuny.edu>
2017-04-25 13:32:51 -0400

    Update migrations.

--- migrations/20170424_create_canned_analyses_table.sql
+++ migrations/20170424_create_canned_analyses_table.sql
@@ -7,11 +7,11 @@ CREATE TABLE `canned_analyses` (
   `dataset_accessions_list` varchar(255) NOT NULL,
   `ca_image_url` varchar(255) DEFAULT NULL,
   `tool_name` varchar(255) NOT NULL,
-  `tool_url` int(10) DEFAULT NULL,
+  `tool_url` varchar(255) DEFAULT NULL,
   `canned_analysis_url` varchar(255) NOT NULL,
   `canned_analysis_description` text NOT NULL,
   `metadata` varchar(255) DEFAULT NULL,
-  `count_click` int(10) DEFAULT NULL,
+  `count_click` int(10) DEFAULT 0,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
