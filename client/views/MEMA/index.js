8f53b44badac08459c7137faff1f0fd1c436f2d2
Edward He <edward.he@macaulay.cuny.edu>
2017-05-30 16:53:11 -0400

    Make MEMAboard responsive.

--- client/views/MEMA/index.js
+++ client/views/MEMA/index.js
@@ -11,15 +11,16 @@ export default () => (
     />
     <div className="container">
       <div className="row">
+        <div className={`col-xs-12 ${styles['iframe-wrap']}`}>
+          <iframe
+            id="mema-viz"
+            className={styles.iframe}
+            frameBorder="0"
+            scrolling="no"
+            src="https://maayanlab.github.io/MEMAboard/"
+          />
+        </div>
         <div className="col-xs-12">
-          <div className={styles.wrap}>
-            <iframe
-              className={styles.iframe}
-              frameBorder="0"
-              scrolling="no"
-              src="https://maayanlab.github.io/MEMAboard/"
-            />
-          </div>
           <div className={styles['viz-description']}>
             <h4>Overview:</h4>
             <p>
