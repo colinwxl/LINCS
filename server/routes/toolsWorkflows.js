a87e8c6c6fa21d04c4455354260d7f0873cb562d
Edward He <edward.he@macaulay.cuny.edu>
2017-06-19 10:45:07 -0400

    Test workflow.

--- server/routes/toolsWorkflows.js
+++ server/routes/toolsWorkflows.js
@@ -101,13 +101,14 @@ router.post('/workflows/add', async (ctx) => {
   if (!workflow.type) {
     ctx.throw(400, 'Workflow requires a type');
   }
+  debugger;
   try {
     const wf = await Workflow.forge(workflow).save().then(wfModel => wfModel.toJSON());
     const transporter = nodemailer
       .createTransport(EMAIL_STR);
     const mailOptions = {
       from: 'LINCS@amp.pharm.mssm.edu',
-      to: 'sherry.jenkins@mssm.edu, avi.maayan@mssm.edu, edward.he@mssm.edu, moshe.silverstein@mssm.edu, denis.torre@mssm.edu',
+      to: 'edward.he@mssm.edu',
       subject: 'A new workflow has been submitted',
       text: 'Hello,\n\n' +
         'This is a notification from http://amp.pharm.mssm.edu/LINCS that a ' +
