22dcf0961959a7b8c4e70c7594a3a27d05600431
MosheSilverstein <moshechayimsilverstein@gmail.com>
2017-12-22 11:46:26 -0500

    fixing new news anouncments

--- client/views/NewsView/Stories/Story201712204.js
+++ client/views/NewsView/Stories/Story201712204.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Story from './Story';
 import styles from '../NewsView.scss';
 
+import photo from './images/dsgcPubPhoto2017.png';
+
 export default function Story20150208() {
   return (
     <Story
@@ -12,7 +14,7 @@ export default function Story20150208() {
       author="Alexandra B. Keenan"
     >
       <img
-        src=" https://oup.silverchair-cdn.com/oup/backfile/Content_public/Journal/nar/PAP/10.1093_nar_gkx1063/1/gkx1063fig2.jpeg?Expires=1513896915&Signature=bPAzn0gYGGaxixm9A9QlK4NZoO62xgjcTQazv2S3SyqRdqbDLIm-53mWcO6~xv54kl0VaCRdxJU5pIf9MoQoDr9vA0P0Lmp1P~1Vp-~5OiaFm6iW8dg0VVNDZTpl9d8nHOHmFY0i98RhbbV6VI7LnqW3~bWhsZnpsBkpqWk7r0eUYys7f5270UvO1PBCyvrKGspNKo9pUyJPWfHS3U6-fWpeWlk0ZB7Bg4JOWYONQlzHR8Qu4NMAtLPpTHkvwWf3F4PyFFeVj7B61aeSniU4GmV~so7RBS7mxdn3XT1izZi5FcKSYbebQKfImDWg~ZU10jAM8SjYs-E~lZIjqPO3Aw__&Key-Pair-Id=APKAIUCZBIA4LVPAVW3Q"
+        src={photo}
         alt="dcic"
         className={styles['inline-img-left']}
         style={{ maxWidth: '25rem', maxHeight: '25rem' }}
